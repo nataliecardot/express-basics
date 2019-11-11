a6c7bb4268115ee4d72b0953503f796b41024f9e
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-10 23:47:02 -0800

    Add route for home

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// Route/endpoint (for get requests, the type of request when you go to a webpage)
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+
 // When it's deployed, server probably won't run on 5000, but will have the port in an environment variable, so we should check that first, otherwise run on 5000
 const PORT = process.env.PORT || 5000;
 

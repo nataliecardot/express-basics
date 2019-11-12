5a4d4a577981c065fb655bb84fe4e550e9d581de
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-11 20:55:50 -0800

    Move model/JSON data to its own file

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
 const express = require('express');
 const path = require('path');
+const members = require('./members');
 
 const app = express();
-
-const members = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'john@gmail.com',
-    status: 'active'
-  },
-  {
-    id: 2,
-    name: 'Bob Williams',
-    email: 'bob@gmail.com',
-    status: 'inactive'
-  },
-  {
-    id: 3,
-    name: 'Shannon Jackson',
-    email: 'shannon@gmail.com',
-    status: 'active'
-  }
-];
 
 // Return members JSON for get request from this route/endpoint
 app.get('/api/members', (req, res) =>

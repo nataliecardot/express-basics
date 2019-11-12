d8e6ed0af791f7510cb22c7101184b21d659093c
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-11 20:20:44 -0800

    Set static folder, add static files and CSS

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-// Route/endpoint (for get requests, the type of request when you go to a webpage)
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
+// For static server that serves regular HTML files and CSS
+// Set static folder (use is method we use when we want to include middleware). We can put multiple HTML files and CSS in that folder and Express handles things like setting the content type and loading the files for us
+app.use(express.static(path.join(__dirname, 'public')));
 
 // When it's deployed, server probably won't run on 5000, but will have the port in an environment variable, so we should check that first, otherwise run on 5000
 const PORT = process.env.PORT || 5000;

5e44e2ad28f9626ace79f39bad18f119684d385d
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-12 22:17:27 -0800

    Get single member JSON

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const logger = require('./middleware/logger');
 const app = express();
 
 // Initialize middleware
-app.use(logger);
+// app.use(logger);
 
 // Return members JSON for get request from this route/endpoint
 app.get('/api/members', (req, res) =>
   // When sending data to a web server, the data has to be a string. members variable holds a JOSn object but should be a JSON string representation. However, don't need to use JSON.stringify() when using Express; with res.json(), the conversion is done for you. It does JSON.stringify(object, replacer, space), and once this method is called, the res.json() method will then call res.send() as well, under the hood
   res.json(members));
+
+// Get single member JSON
+/* Route parameters are named URL segments used to capture the values specified at their position in the URL. The named segments are prefixed with a colon and then the name (e.g. /:your_parameter_name/. The captured values are stored in the req.params object using the parameter names as keys (e.g., req.params.your_parameter_name). https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes */
+app.get('/api/members/:id', (req, res) => {
+  // Have to wrap req.params.id in parseInt because req.params.id sends it as a string (triple equals used so types must match)
+  res.json(members.filter(member => member.id == parseInt(req.params.id)));
+});
 
 // For static server that serves regular HTML files and CSS (although typically you won't be using Express for this, but will be building JSON APIs to connect from a front end like React or render templates in which you insert dynamic data in order to have a dynamic app rather than just a static website)
 // Set static folder (use is method we use when we want to include middleware). We can put multiple HTML files and CSS in that folder and Express handles things like setting the content type and loading the files for us
